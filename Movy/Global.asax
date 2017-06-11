a4a99be2f2b3546deb466ffaddb67e581c70f6ac
Eirik Strom <eirikstrom1@gmail.com>
2017-06-11 21:05:50 +0200

    Add project files.

---
+++ Movy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movy.MvcApplication" Language="C#" %>
---
+++ Movy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movy.Startup))]
+namespace Movy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
