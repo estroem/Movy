e0cee8c0377c6baaf84f760da89308cbfe7168eb
Eirik Strom <eirikstrom1@gmail.com>
2017-06-13 15:02:40 +0200

    Add admin role in Configurations.cs

--- Movy/Migrations/Configuration.cs
+++ Movy/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace Movy.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,6 +18,21 @@ namespace Movy.Migrations
 
         protected override void Seed(Movy.Models.ApplicationDbContext context)
         {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                context.Roles.Add(new IdentityRole { Name = "Admin" });
+                context.SaveChanges();
+
+                var user = userManager.FindByEmail("admin@h.com");
+
+                if (user != null)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
