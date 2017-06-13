7a9581305d6840c42e6768e463f7afe71803de9f
Eirik Strom <eirikstrom1@gmail.com>
2017-06-13 12:18:36 +0200

    Implement search feature

--- Movy/Controllers/SearchController.cs
+++ Movy/Controllers/SearchController.cs
@@ -8,12 +8,25 @@ using Movy.Models;
 namespace Movy.Controllers
 {
     public class SearchController : Controller
-    {        
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index(string s)
         {
+            ViewBag.NoMatch = false;
+
             if (String.IsNullOrWhiteSpace(s))
                 return View();
-            return Content(s);
+
+            var movie = db.Movies.FirstOrDefault(m => m.Name == s);
+
+            if (movie == null)
+            {
+                ViewBag.NoMatch = true;
+                return View();
+            }
+
+            return RedirectToAction("Details", "Movies", new { id = movie.Id });
         }
     }
 }
